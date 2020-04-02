2b1bb18725e693301f4e1b9f9d220ae9475f785b
gongluck <1039994845@qq.com>
2020-04-02 20:37:39 +0800

    调整脚本

--- build_win.bat
+++ build_win.bat
@@ -2,7 +2,6 @@
 cd %~dp0
 call del_win.bat
 for /D %%s in (*) do ( 
-echo %%s
 cd %%s
 go mod init "%%s"
 go build
--- del_win.bat
+++ del_win.bat
@@ -2,10 +2,8 @@
 cd %~dp0
 for /D %%s in (*) do ( 
 cd %%s
-echo %%s
 del /s /q *.exe
 if exist go.mod (
-echo del go.mod
 del go.mod
 )
 cd .. 
